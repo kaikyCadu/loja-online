fe38a332d6145900852e260083a4b697d6383362
unknown <kaikyvanced@gmail.com>
2026-09-07 12:50:54 -0300

    fix: criacao da logica inicial

---
+++ app.js
@@ -0,0 +1,15 @@
+
+
+
+function calculartotal (itens) {
+    let total = 0
+
+    for (let i = o; < itens.length; i++){
+        total += itens [i].preco
+    }
+
+    //aplica desconto de fidelidade
+    //antes de retornar o valor final
+
+    return total
+}
